845301fdf4347ab5f53ce0357db29062ac4d5549
0num4 <r.oonuma@matsuri-tech.com>
2024-06-23 21:39:26 +0900

    feat: add sample

--- poetry_stabs_package_sample/sample.py
+++ poetry_stabs_package_sample/sample.py
@@ -1,19 +1,22 @@
-# lambdaに型を付けるテスト
+"""lambdaに型を付けるテスト."""
+
 from typing import Callable
 
 
 def main() -> None:
+    """メイン関数."""
     # def test_func(func: Callable[[int, int], int]) -> None:
     #     print(func(1, 2))
 
     # def add(a: int, b: int) -> int:
     #     return a + b
-    l: Callable[[int], int] = lambda x: x + 1
+    l: Callable[[int], int] = lambda x: x + 1  # noqa: E731, E741
     l(34)
     nandemo(4, 6)
 
 
 def nandemo(*args, **kwargs) -> None:
+    """可変長引数のテスト関数."""
     print(len(args))
     print(len(kwargs))
 

321d5d79dd83cdc6a5f7dddb6c9e00cd78af06ed
0num4 <r.oonuma@matsuri-tech.com>
2024-06-23 21:36:13 +0900

    feat: ruff check . --fix

--- poetry_stabs_package_sample/__init__.py
+++ poetry_stabs_package_sample/__init__.py
@@ -1,6 +1,5 @@
 # https://github.com/psf/requests/blob/main/src/requests/__init__.py#L143
 # requestsは相対インポート使ってるな
-from . import sample
-from . import sample2
+from . import sample, sample2
 
 __all__ = ["sample", "sample2"]
